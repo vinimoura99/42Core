9fd789d8e018414f01dd4c7304b43c4c725656d3
Vinicius Moura <vinicius_moura98@yahoo.com>
2025-04-22 08:44:12 +0100

    done

--- Libft/ft_memccpy.c
+++ Libft/ft_memccpy.c
@@ -32,3 +32,5 @@ void	*ft_memccpy(void *dst, const void *src, int c, size_t n)
 	}
 	return (NULL);
 }
+
+
--- Libft/ft_strchr.c
+++ Libft/ft_strchr.c
@@ -27,3 +27,5 @@ char	*ft_strchr(const char *s, int c)
 		return ((char *)(s + i));
 	return (NULL);
 }
+
+
